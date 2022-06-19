00657b5f8cde60c433e5c648dbc51d32b179c9e2
RobinTeotia <62256462+RobinTeotia@users.noreply.github.com>
2022-06-19 02:33:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+server = Flask(__name__)
 
 
-@app.route("/")
+@server.route("/")
 def hello_world():
     return render_template("dash.py")
